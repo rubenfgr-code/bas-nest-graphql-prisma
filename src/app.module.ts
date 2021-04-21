cd0f3a7edcbb462bee09b0234c4dfe7c2b4c4b4a
rubenfgr <rubenfgr87@outlook.com>
2021-04-21 10:11:09 +0200

    graphql configuration and generated resources to user and profile

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,27 @@
 import { Module } from '@nestjs/common';
-import { ConfigModule } from '@nestjs/config';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import { GraphQLModule } from '@nestjs/graphql';
+import { join } from 'path';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { AuthModule } from './modules/auth/auth.module';
 
 @Module({
-  imports: [ConfigModule.forRoot({ isGlobal: true, cache: true })],
+  imports: [
+    ConfigModule.forRoot({ isGlobal: true, cache: true }),
+    GraphQLModule.forRootAsync({
+      imports: [ConfigModule],
+      useFactory: async (configService: ConfigService) => ({
+        autoSchemaFile: join(
+          process.cwd(),
+          configService.get<string>('GRAPHQL_SCHEMAFILE'),
+        ),
+        sortSchema: configService.get<boolean>('GRAPHQL_SORTSCHEMA'),
+      }),
+      inject: [ConfigService],
+    }),
+    AuthModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
